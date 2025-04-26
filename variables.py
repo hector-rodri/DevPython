7ee48df6ae1147df93a72e00a5d03ae4c391fd7d
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-26 14:44:06 +0200

    otras operaciones

--- variables.py
+++ variables.py
@@ -5,5 +5,11 @@ numero1 = int(numero1)
 numero2 = int(numero2)
 
 suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
 
 print("La suma de tus números es:",suma)
+print("La resta de los número es",resta)
+print("La multiplicación de los número es",multiplicacion)
+print("La división de los número es",division)
