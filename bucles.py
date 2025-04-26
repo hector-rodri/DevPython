180bc19b8c691196abc7af58df974305ef4b114b
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-26 15:43:04 +0200

    print

--- bucles.py
+++ bucles.py
@@ -4,6 +4,7 @@ rango = input("Dime hasta que número quieres multiplicar al anterior número")
 num1 = int(num1)
 rango = int(rango)
 
+print("TABLA DEL",num1)
 for i in range(1,rango+1):
     resultado = num1*i
     print(num1,"x",i,"=",resultado)
