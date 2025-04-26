70916b46cca781af8481dd752dcb77c4165d109a
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-26 14:41:56 +0200

    ejercicio para entender sintaxis de las variables y su conversión

---
+++ variables.py
@@ -0,0 +1,9 @@
+numero1 = input("Introduce un primer número")
+numero2 = input("Introduce un segundo número")
+
+numero1 = int(numero1)
+numero2 = int(numero2)
+
+suma = numero1 + numero2
+
+print("La suma de tus números es:",suma)
