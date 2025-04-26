0dec1bd055ab8ebdf31ad47e3b53ac7f3c7b122e
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-26 16:36:59 +0200

    practica para aprender a utilizar listas

---
+++ listas.py
@@ -0,0 +1,9 @@
+mis_frutas = ["naranja","platano","sandía","uva","kiwi"]
+
+print(mis_frutas[0])
+print(mis_frutas[4])
+
+mis_frutas.append("melón")
+mis_frutas.remove("uva")
+mis_frutas.sort()
+print(mis_frutas)
