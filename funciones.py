00dffd7e643789030450bcde5c64a20fde7cbeb7
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-26 15:59:23 +0200

    función doble

--- funciones.py
+++ funciones.py
@@ -1,8 +1,12 @@
 def cuadrado(numero):
     return numero * numero
 
+def doble(numero):
+    return numero * 2
+
 num1 = input("Introduce un número ")
 
 num1 = int(num1)
 
-print(cuadrado(num1))
+print("El cuadrado de",num1,"es",cuadrado(num1))
+print("El doble de",num1,"es",doble(num1))
