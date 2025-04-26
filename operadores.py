82ce70c476dc49d536fa511bdf78f0c675d45519
hector-rodri <hrodriguez@nigul.cide.es>
2025-04-26 14:48:45 +0200

    practica para aprender a usar los operadores en python

---
+++ operadores.py
@@ -0,0 +1,12 @@
+num1 = input("Introduce un primer número")
+num2 = input("Introduce un segundo número")
+
+num1 = int(num1)
+num2 = int(num2)
+
+if num1 > num2:
+    print("El primer número es mayor")
+elif num2 > num1:
+    print("El segundo número es mayor")
+else:
+    print("Los números son iguales")
