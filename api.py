feed444eebfbe44c598ae7252b9baf0dfc07fcde
hector-rodri <hrodriguez@nigul.cide.es>
2025-06-04 21:42:40 +0200

    request api pokemon for know info

--- api.py
+++ api.py
@@ -1,8 +1,19 @@
 import requests
 
-URL = "https://pokeapi.co/api/v2/pokemon/"
+nombre = input('Write the name of the pokemon to know its info: ').lower()
+URL = f"https://pokeapi.co/api/v2/pokemon/{nombre}"
 
 respone = requests.get(URL)
-data = respone.json()
 
-print(data)
+if respone.status_code == 200:
+    data = respone.json()
+    print('Name: '+data["name"])
+    tipos = [t["type"]["name"] for t in data["types"]]
+    print("Type: " + ", ".join(tipos))
+    height = data["height"] / 10
+    print(f"Height: {height} m")
+    weight = data["weight"] / 10
+    print(f"Weight: {weight} kg")
+else:
+    print('Pokemon nof found')
+
