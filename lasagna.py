4990e96514e39241948e540f38042b2c40eb52c9
hector-rodri <hrodriguez@nigul.cide.es>
2025-07-22 18:58:24 +0200

    Guido's Gorgeous Lasagna

---
+++ lasagna.py
@@ -0,0 +1,11 @@
+EXPECTED_BAKE_TIME = 30
+PREPARATION_TIME = 2
+
+def bake_time_remaining(elapsed_bake_time):
+    return elapsed_bake_time - EXPECTED_BAKE_TIME
+
+def preparation_time_in_minutes(layers):
+    return layers * PREPARATION_TIME
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
