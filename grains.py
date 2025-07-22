2bdbfa34e0ae2851b46cc9cccda01fd4a5910cce
hector-rodri <hrodriguez@nigul.cide.es>
2025-07-22 21:15:09 +0200

    lore

--- grains.py
+++ grains.py
@@ -6,6 +6,11 @@ def grains_on_square(square):
 def total_grains_on_board():
     return 2 ** 64 - 1
 
+print('There once was a wise servant who saved the life of a prince.')
+print('The king promised to pay whatever the servant could dream up.')
+print('Knowing that the king loved chess, the servant told the king he would like to have grains of wheat.') 
+print('One grain on the first square of a chessboard, with the number of grains doubling on each successive square.')
+
 try:
     square = int(input("Enter a square number (1-64): "))
     grains = grains_on_square(square)
