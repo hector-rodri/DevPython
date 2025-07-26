a37bb3a53d330d718f20c2db322b655ce4a09b0e
hector-rodri <hrodriguez@nigul.cide.es>
2025-07-26 18:45:18 +0200

    lore

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,4 +1,15 @@
-number = int(input('Write a number: '))
+print("One evening, you stumbled upon an old notebook filled with cryptic scribbles,")
+print("as though someone had been obsessively chasing an idea.")
+print("On one page, a single question stood out: Can every number find its way to 1?")
+print("It was tied to something called the Collatz Conjecture, a puzzle that has baffled thinkers for decades.\n")
+
+print("The rules were deceptively simple. Pick any positive integer.\n")
+
+print("If it's even, divide it by 2.")
+print("If it's odd, multiply it by 3 and add 1.")
+print("Then, repeat these steps with the result, continuing indefinitely.")
+
+number = int(input('Write a number:'))
 
 while True:
     if number % 2 == 0:
