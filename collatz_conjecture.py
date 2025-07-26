fdebe323037c2af7e5c052ddd8d541d45b567603
hector-rodri <hrodriguez@nigul.cide.es>
2025-07-26 18:44:46 +0200

    initial

---
+++ collatz_conjecture.py
@@ -0,0 +1,10 @@
+number = int(input('Write a number: '))
+
+while True:
+    if number % 2 == 0:
+        number  = number / 2
+        print(number)
+    else:
+        number = (number * 3) + 1
+        print(number)
+
